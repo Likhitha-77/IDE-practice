2c060920e2ff8ef02110634ada12ce2aae58716f
Likhitha-77 <59175243+Likhitha-77@users.noreply.github.com>
2020-11-17 21:14:22 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week7
+{
+    public partial class Form1 : Form
+    {
+        public static string SetValueForText1 = "";
+        public static string SetValueForText2 = "";
+        public static string SetValueForText3 = "";
+        public static string SetValueForText4 = "";
+        public static string SetValueForText5 = "";
+        public static string SetValueForText6 = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetValueForText1 = textBox1.Text;
+            SetValueForText2 = textBox2.Text;
+            SetValueForText3 = textBox3.Text;
+            SetValueForText4 = textBox4.Text;
+            SetValueForText5 = textBox5.Text;
+            SetValueForText6 = textBox6.Text;
+            if (Form1.SetValueForText2 == Form1.SetValueForText3)
+            {
+                Form2 frm2 = new Form2();
+                frm2.Show();
+                Form3 frm3 = new Form3();
+                frm3.Show();
+            }
+            else
+            {
+                textBox1.Text = " ";
+                textBox2.Text = " ";
+                textBox3.Text = " ";
+                textBox4.Text = " ";
+                textBox5.Text = " ";
+                textBox6.Text = " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+            textBox4.Text = " ";
+            textBox5.Text = " ";
+            textBox6.Text = " ";
+        }
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week7
+{
+    public partial class Form3 : Form
+    {
+        public static string SetValueForText1 = "";
+        public static string SetValueForText2 = "";
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetValueForText1 = textBox1.Text;
+            SetValueForText2 = textBox2.Text;
+            if(Form3.SetValueForText1==Form1.SetValueForText1 && Form3.SetValueForText2 == Form1.SetValueForText2)
+            {
+                Form4 frm4 = new Form4();
+                frm4.Show();
+            }
+            else
+            {
+                textBox1.Text = " ";
+                textBox2.Text = " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+        }
+    }
+}
